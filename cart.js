31ae81dd1a2e1da2f0a2f53088b11e3f0a493fbc
Hossam-El-Batal <tokyo.hossam1@gmail.com>
2024-07-13 08:41:13 +0300

    finished add to cart function

--- cart.js
+++ cart.js
@@ -1,109 +1,121 @@
-// Initialize the users object and store it in localStorage
-let users = {
-  1: 1
-}
-let cartsIDs = {
-  1: 1
-}
-localStorage.setItem("users", JSON.stringify(users))
-localStorage.setItem("CartId", JSON.stringify(cartsIDs))
 
-// Function to get the cart of the user 
-async function getUserCart() {
-  // Retrieve the users object from localStorage
-  const storedUsers = JSON.parse(localStorage.getItem("users"))
-  // Get the user ID (assumes there's only one user with ID 1)
-  const userId = storedUsers[1]
+// Retrieve object from local storage
+// let localStorageItem = JSON.parse(localStorage.getItem('products'));
+
+// localStorageItem.record.products = [];
+
+
+// localStorage.setItem('products', JSON.stringify(localStorageItem));
+
+
+/*               */
+
+let Update_flag = false;
+
+const cart_queue = [];
+
+  const binId = '668d8601e41b4d34e40f81e7';
+const apiKey = '$2a$10$oFTM9uqaBHpQjTjZZLIUauYv6mAMtPqAgdACQF7TE2jYo91EMBBti';
+
   
-  const data = await fetch('https://dummyjson.com/carts/add', {
-    method: 'PUT',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({
-      userId: userId,
-      products: [
-        {}
-      ]
-    })
-  })
-  const finalData = await data.json()
-  const cartIdData = finalData.id;
-  let cartObject = JSON.parse(localStorage.getItem("CartId"))
+  // get 3 items using fetch api
 
-  cartObject[1] = cartIdData;
-
-  localStorage.setItem("CartId", JSON.stringify(cartObject))
-
-}
-
-getUserCart()
-
-
-// get 3 items using fetch api
 async function getProducts(){
-    const data = await fetch('https://dummyjson.com/products?limit=3')
-    const fetchedData = await data.json()
-    console.log(fetchedData.products)
-    for(let i=0; i<3;i++){
-        displayItems(fetchedData.products[i])
-    }
-    
+  const data = await fetch('https://dummyjson.com/products?limit=10')
+  const fetchedData = await data.json()
+  console.log(fetchedData.products)
+  for(let i=0; i<10;i++){
+      displayItems(fetchedData.products[i])
+  }
+  
+  
 }
 getProducts()
 
 // function to display the added items to the mock page
 function displayItems(data){
-    const item = `
-    <hr>
-    <div>${data.title}</div>
-    <div>${data.price}</div>
-    <img src="${data.images[0]}" width=150px>
-    <button onClick="addToCart(${data.id})" class="add-to-cart">click me to add to cart</button>
-    `
-    document.getElementById("something").innerHTML += item
+  const item = `
+  <hr>
+  <div>${data.title}</div>
+  <div>${data.price}</div>
+  <img src="${data.images[0]}" width="150px">
+  <button onClick="addToCart('${data.id}', '${data.price}','${data.discountPercentage}', '${data.images[0]}', '${data.title.replace(/'/g, "\\'")}','${data.brand}','${data.quantity}')" class="add-to-cart">click me to add to cart</button>
+  `
+  document.getElementById("something").innerHTML += item
 }
-
+ 
 // function  to add item to cart
-async function addToCart(itemID){
+async function addToCart(itemID,itemPrice,itemDiscount,itemImage,itemTitle,itemBrand,itemQuantity){
 
-  const storedCartId = JSON.parse(localStorage.getItem("CartId"))
-  const CartId = storedCartId[1]
-const response = await fetch('https://dummyjson.com/carts/1', {
-  method: 'PUT', 
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    merge: true, // this will include existing products in the cart
-    products: [
-      {
-        id: itemID,
-        quantity: 1,
-      },
-    ]
-  })
-})
+try {
+  // Construct the new product object using function parameters
+  const newProduct = {
+    "id": itemID,
+    "image": itemImage,
+    "price": itemPrice,
+    "title": itemTitle,
+    "discount" : itemDiscount,
+    "brand" : itemBrand,
+    "quantity" : itemQuantity
+  };
 
-const cartdata = await response.json()
-console.log(cartdata);
-      
-}
+  cart_queue.push(newProduct)
 
-
-
-
-  //display function displays added items to the cart page
-  function displayAddedItem(data){
-  
-    const item = `
-         <img src="${data.image}">
-         <span>${data.title}</span>
-         <span>${data.price}</span>
-         <span>${data.quantity}</span>
-         <img class="remove-item" src="delete.svg"></div>
-    `
-    document.getElementById("items").innerHTML += item
+  if(Update_flag== false){
+    Update_flag = true
+    await process_queue();
+  }}
+  catch (error){
+    console.error("error in add to cart: ", error.message)
   }
 
-  //
+}
+async function  process_queue(){
 
+  try {
+    while (cart_queue.length > 0) {
+      const newProduct = cart_queue.shift();
+  // Fetch current data from JSONBin.io
+  const response = await fetch(`https://api.jsonbin.io/v3/b/${binId}/latest`, {
+    headers: {
+      'X-Master-Key': apiKey
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to fetch data from JSONBin.io');
+  }
+
+  const currentData = await response.json();
+  console.log('Current JSON data:', currentData);
+
+  // Update the products array with the new product
+  currentData.record.products.push(newProduct);
+
+  // Update JSONBin.io with the modified data
+  const updateResponse = await fetch(`https://api.jsonbin.io/v3/b/${binId}`, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Master-Key': apiKey
+    },
+    body: JSON.stringify(currentData.record)
+  });
+
+  if (!updateResponse.ok) {
+    throw new Error('Failed to update data on JSONBin.io');
+  }
+
+  const updatedData = await updateResponse.json();
+  // UPDATE LOCAL STORAGE WITH THE LATEST DATA
+  localStorage.setItem("products", JSON.stringify(updatedData))
+  Update_flag = false;
+
+}} catch (error) {
+  console.error('Error updating data on JSONBin.io:', error.message);
+}
+displayAddedItem()
+}
 
 
 
